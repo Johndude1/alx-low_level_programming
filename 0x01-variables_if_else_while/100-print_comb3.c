c85602f0421d50a0c82b42762fd11df7d5a6f23b
Johndude1 <olajideleke@gmail.com>
2023-07-17 19:21:39 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,25 +11,24 @@ int main(void)
 	int i = '0';
 	int j = '0';
 
-	while(i <= '9')
+	while (i <= '9')
 	{
 		while (j <= '9')
 		{
-			if(!(i > j) || i == j)
+			if (!(i > j) || i == j)
 			{
 				putchar(i);
 				putchar(j);
-				if (i== '8' && j == '9')
+			if (i == '8' && j == '9')
 				{
-					putchar('\n');
+				putchar('\n');
 				}
 				else
 				{
-					putchar(',');
-					putchar(' ');
+				putchar(',');
+				putchar(' ');
 				}
 			}
-j++;
 		}
 	}
 j = '0';
