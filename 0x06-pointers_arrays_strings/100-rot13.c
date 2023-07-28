a598eae8222faeb017e7adb7b27add71b19c1e84
Johndude1 <olajideleke@gmail.com>
2023-07-28 07:27:03 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*root13 - encider rot13
+*@s: pointer to string params
+*Return: *s
+*/
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char data i[] =
+		char datarot[]=
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			for (j = 0; j < 52; j++)
+			{
+				if (s[i] == data i[s])
+				{
+					s[i] = dataroot [j];
+					break;
+				}
+			}
+		}
+	return (s);
+}
