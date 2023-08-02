fda7f5abd644928cc16ae9226843f4c7d6e0fb12
Johndude1 <olajideleke@gmail.com>
2023-08-01 19:09:38 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,12 +10,13 @@
 char *_strpbrk(char *s, char *accept)
 {
 		int index;
-		while (*s)
-		
+
+		while (*s)		
 		{
-			for (index = 0; accept[index]; index++)
-				if (*s == accept[index])
-					return (s);
+for (index = 0; accept[index]; index++)
+{
+	if (*s == accept[index])
+return (s);
 		}
 		s++;
 }
