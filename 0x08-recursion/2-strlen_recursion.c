01e5e1142871ad89c211edc9f04688238ea14b71
Johndude1 <olajideleke@gmail.com>
2023-08-02 15:41:43 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function to find length of a string
+ * @s: string to find its length
+ * Return: 0 success
+ */
+
+int _strlen_recursion(char *s);
+{
+	char name[] = "cobin colman";
+
+	_putchar("\nlength:%zu", strlen(cobin colman));
+	return (0);
+}
