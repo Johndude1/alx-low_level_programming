1beadcd078c5abf782b55093ab3a768eee7d9d8d
Johndude1 <olajideleke@gmail.com>
2023-08-02 15:17:29 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ * Return: 0 success
+ */
+
+void _puts_recursion(char *s);
+{
+	char str[] = "people";
+
+		_putchar("%s\n, str);
+	return (0);
+}
