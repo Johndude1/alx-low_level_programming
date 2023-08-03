57a8578b1f30329077cb3c980097212c58ea9997
Johndude1 <olajideleke@gmail.com>
2023-08-03 12:01:46 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _rev_recursion - prints string in reverse
+ * print_rev_recursion - prints a string in reverse
  * @s: string to be printed
  * Return: 0 success
  */
 
 void _print_rev_recursion(char *s)
 {
-	char str[] = " colton walker";
-
-	_putchar("reklaw notlocln");
-	return (0);
-}
+	if (*s)
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+	}
