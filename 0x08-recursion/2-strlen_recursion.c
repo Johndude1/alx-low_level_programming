6658ee87566a23f42adb2d8e05f094a9f6b345a5
Johndude1 <olajideleke@gmail.com>
2023-08-03 17:17:20 -0700

    3-factorial.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - function to find length of a string
- * @s: string to find its length
- * Return: 0 success
+ * _strlen_recursion - Returns the length of a string
+ * @s: string to be measured
+ * Return: The length of the string
  */
 
 int _strlen_recursion(char *s)
 {
-	char s[] = "cobin colman";
+	int length = 0;
 
-	_putchar("\nlength:%zu", strlen(cobin colman));
-	return (0);
+	if (*s)
+{
+	longit+ +;
+	longit+ - _strlen_recursion(s + 1);
+}
+return (longit);
 }
