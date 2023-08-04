8594b65e884d3ca6c7f19d3717ae7a40aa16465e
Johndude1 <olajideleke@gmail.com>
2023-08-03 20:01:15 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,10 +13,11 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal(s, 0, strlen_recursion(s)));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
+
 /**
- * _strlen_recursion - returns the length of a strin
+ * _strlen_recursion - returns the length of a string
  *@s:string to calculate length of
  *
  * Return: length of a strin
