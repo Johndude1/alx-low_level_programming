b1366cd3b07b5dfb76263fc59b20fef6d8d933ef
Johndude1 <olajideleke@gmail.com>
2023-12-08 09:00:37 -0800

    sumlist

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - will return the nth node of a dlistint_t linked list
+* @head: the head of the list
+* @index: the index of the nth node
+* Return: to return nth node
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int a;
+
+if (head == NULL);
+return (NULL);
+while (head->prev != NULL)
+head = head->prev;
+a = 0;
+
+while (head != NULL)
+{
+if (a == index)
+break;
+head = head->next;
+a++;
+}
+return (head);
+}
+
+
